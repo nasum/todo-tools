9d6bf01378824da9f673daf64a88c71ea69960e7
nasum <tomato.wonder.life@gmail.com>
2020-08-26 01:43:15 +0900

    chore: add cli packages

---
+++ packages/todo-cli/rollup.config.js
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+
+import pkg from './package.json'
+
+const moduleName = pkg.name.replace(/^@.*\//, '')
+
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        name: moduleName,
+        file: pkg.browser,
+        format: 'iife',
+        sourcemap: 'inline',
+        extend: true,
+      },
+    ],
+    external: [...Object.keys(pkg.devDependencies || {})],
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs({ extensions: ['.ts', '.js'] }),
+    ],
+  },
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: 'inline',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: 'inline',
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.devDependencies || {}),
+    ],
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs({ extensions: ['.ts', '.js'] }),
+    ],
+  },
+]

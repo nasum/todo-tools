373271b287be0f5fee1181bd0acb1e54c9f80c00
nasum <tomato.wonder.life@gmail.com>
2020-08-30 23:01:43 +0900

    chore: added setting load func

--- packages/todo-cli/src/todo.ts
+++ packages/todo-cli/src/todo.ts
@@ -1,6 +1,11 @@
+import Configstore from 'configstore'
+import { Config, DefaultConfig } from './config'
 import { createCommand } from 'commander'
 import { makeAddCommand } from './commands/add'
 import { makeLsCommand } from './commands/ls'
+
+const packageJson = require('../package.json')
+const config: Config = new Configstore(packageJson.name, DefaultConfig).all
 
 const program = createCommand()
 
@@ -8,7 +13,7 @@ program.action(() => {
   console.log('todo')
 })
 
-program.addCommand(makeAddCommand())
+program.addCommand(makeAddCommand(config))
 program.addCommand(makeLsCommand())
 
 program.version('0.0.1')

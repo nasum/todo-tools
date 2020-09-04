002cd417f61eb5983fd190bf41cf61f715f179b3
nasum <tomato.wonder.life@gmail.com>
2020-09-04 22:38:16 +0900

    feat: add done command

---
+++ packages/todo-cli/src/commands/done.ts
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import readline from 'readline'
+import { createCommand } from 'commander'
+import { Config } from '../config'
+import { ConfigUtil } from '../lib/configUtil'
+
+export function makeDoneCommand(config: Config) {
+  const done = createCommand('done')
+  const cUtil = new ConfigUtil(config)
+
+  done.action((_, target) => {
+    target = target.map((numberSt: string) => Number(numberSt))
+    if (fs.existsSync(cUtil.todoFilePath())) {
+      const rl = readline.createInterface({
+        input: fs.createReadStream(cUtil.todoFilePath()),
+      })
+      let text = ''
+      let n = 0
+      rl.on('line', function (line) {
+        if (target.indexOf(n) < 0) {
+          text += line
+          text += '\n'
+        } else {
+          text += `x ${line}`
+          text += '\n'
+        }
+        n += 1
+      })
+
+      rl.on('close', function () {
+        fs.open(cUtil.todoFilePath(), 'w', (err, fd) => {
+          if (err) throw err
+          fs.writeFile(fd, text, (err) => {
+            if (err) throw err
+            fs.close(fd, (err) => {
+              if (err) throw err
+            })
+          })
+        })
+      })
+    } else {
+      console.log(`not exist ${cUtil.todoFilePath()}`)
+    }
+  })
+  return done
+}
--- packages/todo-cli/src/todo.ts
+++ packages/todo-cli/src/todo.ts
@@ -4,6 +4,7 @@ import { createCommand } from 'commander'
 import { makeAddCommand } from './commands/add'
 import { makeLsCommand } from './commands/ls'
 import { makeRmCommand } from './commands/rm'
+import { makeDoneCommand } from './commands/done'
 
 const packageJson = require('../package.json')
 const config: Config = new Configstore(packageJson.name, DefaultConfig).all
@@ -17,6 +18,7 @@ program.action(() => {
 program.addCommand(makeAddCommand(config))
 program.addCommand(makeLsCommand(config))
 program.addCommand(makeRmCommand(config))
+program.addCommand(makeDoneCommand(config))
 
 program.version('0.0.1')
 program.parse(process.argv)
